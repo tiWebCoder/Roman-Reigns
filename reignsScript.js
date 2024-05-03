f6c1a63f1ccb02edbb6f9d25d92a21e1a5a8a309
tiWebCoder <127739121+tiWebCoder@users.noreply.github.com>
2024-05-03 14:07:46 +0100

    Add files via upload

---
+++ reignsScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    /**
+     * Creates a modal window which displays a maintenance message
+     */
+    $('.btn-link').click(function(){
+        alert('This content is currently undergoing maintenance and will be available soon');
+    });
+
+
+});
